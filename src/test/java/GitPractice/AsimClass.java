c28721377ffbcf0b258d41e0462c01420ee3e879
Asım Çelik <you@example.com>
2022-06-22 18:43:05 +0100

    asims first method

--- src/test/java/GitPractice/AsimClass.java
+++ src/test/java/GitPractice/AsimClass.java
@@ -3,5 +3,9 @@ package GitPractice;
 public class AsimClass {
     public static void main(String[] args) {
         System.out.println("Asims Class");
+        System.out.println("hello from wednesday");
+    }
+    public static void doSomething(){
+        System.out.println("first method");
     }
 }
