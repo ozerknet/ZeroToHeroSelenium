cfa3a2e708ecb3775b75158fb33186520f46b625
vasylmaziar <myzjar@gmail.com>
2022-06-22 22:47:52 +0300

    VasylsCommit

--- src/test/java/GitPractice/VasylsClass.java
+++ src/test/java/GitPractice/VasylsClass.java
@@ -1,7 +1,13 @@
 package GitPractice;
 
 public class VasylsClass {
+
     public static void main(String[] args) {
         System.out.println("Infernal Git)))");
+
+
+    }
+    public void infernalGit(){
+        System.out.println("I don't know what is going on");
     }
 }
